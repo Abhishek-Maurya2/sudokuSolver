2f155c792803664685d7e1c409e1fd5c31920cc1
Abhishek-Maurya2 <208akmaurya@hmail.com>
2024-07-11 14:06:11 +0530

    modified code and ppt

--- SudokuGUI.java
+++ SudokuGUI.java
@@ -1,21 +1,59 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class SudokuGUI extends JPanel {
     private static final int SIZE = 9;
     private int[][] board;
+    private Set<Point> rowHighlights = new HashSet<>();
+    private Set<Point> colHighlights = new HashSet<>();
+    private Set<Point> subgridHighlights = new HashSet<>();
+    private JButton startButton;
+
+    private void startSolver() {
+        SudokuSolver solver = new SudokuSolver();
+        new Thread(() -> {
+            solver.solveSudokuWithGUI(board, this);
+        }).start();
+    }
 
     public SudokuGUI(int[][] board) {
         this.board = board;
         JFrame frame = new JFrame("Sudoku Solver");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 600);
-        frame.add(this);
+        frame.setSize(600, 650);
+        frame.setLayout(new BorderLayout());
+
+        this.setPreferredSize(new Dimension(600, 600));
+        frame.add(this, BorderLayout.CENTER);
+
+        startButton = new JButton("Start");
+        frame.add(startButton, BorderLayout.SOUTH);
+
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                startSolver();
+            }
+        });
+
+        frame.pack();
         frame.setVisible(true);
     }
 
     public void updateBoard(int[][] board) {
         this.board = board;
+        repaint(); // Repaint the board to reflect changes
+    }
+
+    public void setHighlights(Set<Point> rowHighlights, Set<Point> colHighlights, Set<Point> subgridHighlights) {
+        this.rowHighlights = rowHighlights;
+        this.colHighlights = colHighlights;
+        this.subgridHighlights = subgridHighlights;
         repaint();
     }
 
@@ -24,35 +62,44 @@ public class SudokuGUI extends JPanel {
         super.paintComponent(g);
         int width = getWidth() / SIZE;
         int height = getHeight() / SIZE;
-        // Fill empty cells with a different color
+
+        // Draw cells with different colors
         for (int row = 0; row < SIZE; row++) {
             for (int col = 0; col < SIZE; col++) {
-                if (board[row][col] == 0) {
-                    g.setColor(Color.LIGHT_GRAY);
-                    g.fillRect(col * width, row * height, width, height);
+                if (rowHighlights.contains(new Point(row, col))) {
+                    g.setColor(Color.RED);
+                } else if (colHighlights.contains(new Point(row, col))) {
+                    g.setColor(Color.GREEN);
+                } else if (subgridHighlights.contains(new Point(row, col))) {
+                    g.setColor(Color.BLUE);
+                } else if (board[row][col] != 0) {
+                    g.setColor(Color.WHITE); // Non-zero cells
+                } else {
+                    g.setColor(Color.LIGHT_GRAY); // Empty cells
                 }
-            }
-        }
-        // Draw numbers
-        g.setFont(new Font("TimesRoman", Font.PLAIN, 18));
-        for (int row = 0; row < SIZE; row++) {
-            for (int col = 0; col < SIZE; col++) {
+                g.fillRect(col * width, row * height, width, height);
+                g.setColor(Color.BLACK);
+                g.drawRect(col * width, row * height, width, height);
+
                 if (board[row][col] != 0) {
                     g.setColor(Color.BLACK);
                     g.drawString(String.valueOf(board[row][col]), col * width + width / 2, row * height + height / 2);
                 }
             }
         }
+
         // Draw grid lines
         g.setColor(Color.BLACK);
         for (int i = 0; i <= SIZE; i++) {
             g.drawLine(i * width, 0, i * width, getHeight());
             g.drawLine(0, i * height, getWidth(), i * height);
         }
+
         // Draw thicker grid lines for 3x3 boxes
         Graphics2D g2 = (Graphics2D) g;
         g2.setColor(Color.RED);
         g2.setStroke(new BasicStroke(3)); // Set the stroke width to 3
+
         for (int i = 0; i <= SIZE; i += 3) {
             g2.drawLine(i * width, 0, i * width, getHeight());
             g2.drawLine(0, i * height, getWidth(), i * height);
@@ -61,22 +108,17 @@ public class SudokuGUI extends JPanel {
 
     public static void main(String[] args) {
         int[][] board = new int[][] {
-                { 5, 3, 0, 0, 7, 0, 0, 0, 0 },
+                { 5, 3, 0, 0, 7, 0, 9, 0, 0 },
                 { 6, 0, 0, 1, 9, 5, 3, 4, 0 },
-                { 0, 9, 8, 3, 4, 0, 0, 6, 0 },
+                { 0, 9, 8, 3, 4, 0, 5, 6, 0 },
                 { 8, 5, 9, 7, 6, 0, 0, 0, 3 },
-                { 4, 0, 0, 8, 0, 3, 0, 0, 1 },
+                { 4, 0, 0, 8, 0, 3, 7, 0, 1 },
                 { 7, 0, 0, 0, 2, 0, 8, 5, 6 },
                 { 0, 6, 1, 0, 0, 0, 0, 8, 0 },
                 { 0, 8, 0, 4, 1, 9, 0, 0, 5 },
-                { 0, 0, 0, 0, 8, 0, 0, 7, 9 }
+                { 0, 4, 5, 0, 8, 0, 0, 7, 9 }
         };
 
-        SudokuSolver solver = new SudokuSolver();
-        SudokuGUI gui = new SudokuGUI(board);
-
-        new Thread(() -> {
-            solver.solveSudokuWithGUI(board, gui);
-        }).start();
+        new SudokuGUI(board);
     }
 }
--- SudokuSolver.java
+++ SudokuSolver.java
@@ -1,9 +1,28 @@
+import java.awt.Point;
+import java.util.HashSet;
+import java.util.Set;
+
 public class SudokuSolver {
     private static final int SIZE = 9;
     private static final int SUBGRID_SIZE = 3;
 
-    private boolean isSafe(int[][] board, int row, int col, int num) {
+    private boolean isSafe(int[][] board, int row, int col, int num, SudokuGUI gui) {
+        Set<Point> rowHighlights = new HashSet<>();
+        Set<Point> colHighlights = new HashSet<>();
+        Set<Point> subgridHighlights = new HashSet<>();
+
         for (int x = 0; x < SIZE; x++) {
+            rowHighlights.add(new Point(row, x));
+            colHighlights.add(new Point(x, col));
+            subgridHighlights.add(new Point(row - row % SUBGRID_SIZE + x / SUBGRID_SIZE,
+                    col - col % SUBGRID_SIZE + x % SUBGRID_SIZE));
+            gui.setHighlights(rowHighlights, colHighlights, subgridHighlights);
+            try {
+                Thread.sleep(5); // to visualize the process
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
             if (board[row][x] == num || board[x][col] == num ||
                     board[row - row % SUBGRID_SIZE + x / SUBGRID_SIZE][col - col % SUBGRID_SIZE
                             + x % SUBGRID_SIZE] == num) {
@@ -18,11 +37,11 @@ public class SudokuSolver {
             for (int col = 0; col < SIZE; col++) {
                 if (board[row][col] == 0) {
                     for (int num = 1; num <= SIZE; num++) {
-                        if (isSafe(board, row, col, num)) {
+                        if (isSafe(board, row, col, num, gui)) {
                             board[row][col] = num;
                             gui.updateBoard(board);
                             try {
-                                Thread.sleep(100); // to visualize the process
+                                Thread.sleep(80); // to visualize the process
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
                             }
@@ -32,7 +51,7 @@ public class SudokuSolver {
                             board[row][col] = 0;
                             gui.updateBoard(board);
                             try {
-                                Thread.sleep(100);
+                                Thread.sleep(20);
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
                             }
@@ -44,5 +63,4 @@ public class SudokuSolver {
         }
         return true;
     }
-
 }
